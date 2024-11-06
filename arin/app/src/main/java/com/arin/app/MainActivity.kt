b9f672c3b4d4419de78612a6a0c94dc7e96c4c0f
roya song <roya.song@obigo.com>
2024-11-06 14:13:33 +0900

    add bg image picker

--- arin/app/src/main/java/com/arin/app/MainActivity.kt
+++ arin/app/src/main/java/com/arin/app/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.arin.app
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.Window
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -17,6 +27,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.arin.app.ui.theme.ComarinappTheme
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 class MainActivity : ComponentActivity() {
     //START values
@@ -24,11 +37,16 @@ class MainActivity : ComponentActivity() {
     lateinit var context_: Context
     var mom_number_: String = "01095444074"
     var dad_number_: String = "01099597899"
+    lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    lateinit var view_bg_image_: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getActionBar()!!.setTitle("장아린 전용 앱")
         setContentView(R.layout.main)
-
+        view_bg_image_ = findViewById(R.id.bg)
+        context_ = getApplicationContext();
+        setImageViewImage(getContext().getFilesDir().getPath() + "/arin_bg.png")
+        registerBackgroundBgPicker()
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_option, menu)
@@ -55,6 +73,58 @@ class MainActivity : ComponentActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+    fun registerBackgroundBgPicker() {
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val intent = checkNotNull(result.data)
+                var currentImageUri  = intent.data
+                try {
+                    currentImageUri?.let {
+                        if(Build.VERSION.SDK_INT < 28) {
+                            val bitmap = MediaStore.Images.Media.getBitmap(
+                                this.contentResolver,
+                                currentImageUri
+                            )
+                            view_bg_image_?.setImageBitmap(bitmap)
+                        } else {
+                            val source = ImageDecoder.createSource(this.contentResolver, currentImageUri)
+                            val bitmap = ImageDecoder.decodeBitmap(source)
+                            view_bg_image_?.setImageBitmap(bitmap)
+                            val back_dir_: String = getContext().getFilesDir().getPath()
+                            Log.e(TAG, "save dir : " + back_dir_)//
+                            saveBitmapAsFile(bitmap,back_dir_+"/arin_bg.png")
+                        }
+                    }
+                } catch(e : Exception) {
+                    Log.e(TAG, "ERROR " + e)//
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+    private fun saveBitmapAsFile(bitmap: Bitmap, filepath: String) {
+        val file = File(filepath)
+        var os: OutputStream? = null
+
+        try {
+            file.createNewFile()
+            os = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, os)
+            os.close()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+    }
+    fun setImageViewImage(filepath : String) {
+        val imgFile = File(filepath)
+        if (imgFile.exists()) {
+            val imgBitmap = BitmapFactory.decodeFile(imgFile.absolutePath)
+            view_bg_image_.setImageBitmap(imgBitmap)
+        }
+    }
+    fun getContext(): Context {
+        return context_
     }
 }
 
