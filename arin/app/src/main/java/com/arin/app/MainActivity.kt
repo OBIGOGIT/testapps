fca564c1cd53abae3d6077ca1a3d1e21ef7145b1
roya song <roya.song@obigo.com>
2024-11-06 11:52:41 +0900

    ui work2

--- arin/app/src/main/java/com/arin/app/MainActivity.kt
+++ arin/app/src/main/java/com/arin/app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.arin.app
 
+import android.content.Context
 import android.os.Bundle
 import android.view.Window
 import androidx.activity.ComponentActivity
@@ -15,21 +16,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.arin.app.ui.theme.ComarinappTheme
 
 class MainActivity : ComponentActivity() {
+    //START values
+    var TAG = "ARIN"
+    lateinit var context_: Context
+    var mom_number_: String = "01095444074"
+    var dad_number_: String = "01099597899"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         getActionBar()!!.setTitle("장아린 전용 앱")
         setContentView(R.layout.main)
-        /*enableEdgeToEdge()
-        setContent {
-            ComarinappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }*/
+
     }
 }
 
