be313f6a5b8064006a2e0ced2243f6be6aa1d432
roya song <roya.song@obigo.com>
2024-11-07 16:01:42 +0900

    add all buttn

--- arin/app/src/main/java/com/arin/app/EditSmsActivity.kt
+++ arin/app/src/main/java/com/arin/app/EditSmsActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,11 +19,12 @@ class EditSmsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContentView(R.layout.activity_edit_sms)
+        setEditSmsTextContent()
         var bSmsapply = findViewById<Button>(R.id.btn_smsApply)
         bSmsapply!!.setOnClickListener(View.OnClickListener {
             Log.e(TAG, "user sms text apply")
-            EditSmsTextApply()
-            finish()
+            if(EditSmsTextApply())
+                finish()
         })
         /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -31,16 +33,32 @@ class EditSmsActivity : AppCompatActivity() {
             insets
         }*/
     }
-    fun EditSmsTextApply() {
+
+    fun setEditSmsTextContent() {
+        var smsArray = SmsTextValue.getInstance().getText();
+        findViewById<EditText>(R.id.editText_sms0).setText(smsArray[0].toString())
+        findViewById<EditText>(R.id.editText_sms1).setText(smsArray[1].toString())
+        findViewById<EditText>(R.id.editText_sms2).setText(smsArray[2].toString())
+        findViewById<EditText>(R.id.editText_sms3).setText(smsArray[3].toString())
+    }
+    fun EditSmsTextApply():Boolean {
         var sms0 = findViewById<EditText>(R.id.editText_sms0)
         var sms1 = findViewById<EditText>(R.id.editText_sms1)
         var sms2 = findViewById<EditText>(R.id.editText_sms2)
         var sms3 = findViewById<EditText>(R.id.editText_sms3)
+        if(sms0.text.toString().isEmpty() ||
+            sms1.text.toString().isEmpty()||
+            sms2.text.toString().isEmpty()||
+            sms3.text.toString().isEmpty()) {
+            Toast.makeText( getApplicationContext(),"빈칸이 있어요..", Toast.LENGTH_SHORT).show()
+            return false;
+        }
         var smsArray = arrayOf(sms0.text.toString(),
                                   sms1.text.toString(),
                                   sms2.text.toString(),
                                   sms3.text.toString())
 
         SmsTextValue.getInstance().setSmsText(smsArray)
+        return true;
     }
 }
