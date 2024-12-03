56605d123c7346ddd594ecafd4ee22b900f3b5c3
roya song <roya.song@obigo.com>
2024-12-03 14:39:22 +0900

    add package check

--- customtab/app/src/main/java/com/roya/customtab/MainActivity.kt
+++ customtab/app/src/main/java/com/roya/customtab/MainActivity.kt
@@ -1,35 +1,46 @@
 package com.roya.customtab
 
 import android.content.Context
+import android.content.pm.PackageInfo
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.roya.customtab.ui.theme.CustomtabTheme
 
+
 class MainActivity : ComponentActivity() {
     lateinit var context_: Context
+    lateinit var btn_open_: Button
+    lateinit var txtview_log_: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layout)
-        context_ = getApplicationContext();
-        var btn_customtab_open : Button = findViewById<Button>(R.id.customtab_open)
-
-        btn_customtab_open.setOnClickListener {
-
+        context_ = getApplicationContext()
+        btn_open_ = findViewById<Button>(R.id.btn_open)
+        txtview_log_ = findViewById<TextView>(R.id.txtview_log)
+        btn_open_.setOnClickListener {
+            var enable = isPackageInstalled(context_,"com.obigo.automotivebrowser" )
+            txtview_log_.setText("isChromeEnabled " + enable)
         }
 
     }
-    fun isChromeEnabled(context: Context): Boolean
-            = context.packageManager.getPackageInfo("com.android.chrome", 0)!!.applicationInfo!!.enabled
+    fun isPackageInstalled(ctx: Context, packageName: String): Boolean {
+        return try {
+            txtview_log_.setText("isChromeEnabled " + 40)
+            packageManager.getPackageInfo(packageName, 0)
+            true
+        } catch (e: Exception) {
+            false
+        }
+
+    }
 }
 
 @Composable
