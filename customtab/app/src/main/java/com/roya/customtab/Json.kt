5cb7e9a5c3ba2edae7e9fc727f9d16ffb203fd24
roya song <roya.song@obigo.com>
2024-12-17 12:12:44 +0900

    test

---
+++ customtab/app/src/main/java/com/roya/customtab/Json.kt
@@ -0,0 +1,23 @@
+package com.roya.customtab
+
+import android.widget.EditText
+
+class Json {
+    lateinit var inputWidth: String
+    lateinit var inputHeight: String
+    lateinit var inputZoomfactor: String
+    lateinit var inputHosturl: String
+    lateinit var inputWhitelist: String
+    lateinit var inputUserAgent: String
+
+    fun setUIJsonData() {
+
+    }
+
+    fun parsingJsonData(json : String) {
+
+    }
+    fun MakeJsonData(json : String) {
+
+    }
+}
