852f8a3e5b5f8b4ae001320ec45e411c68ccfb88
roya song <roya.song@obigo.com>
2025-01-21 14:31:57 +0900

    add json dev

--- customtab/app/src/main/java/com/roya/customtab/Json.kt
+++ customtab/app/src/main/java/com/roya/customtab/Json.kt
@@ -1,23 +1,46 @@
 package com.roya.customtab
 
-import android.widget.EditText
 
-class Json {
-    lateinit var inputWidth: String
-    lateinit var inputHeight: String
-    lateinit var inputZoomfactor: String
-    lateinit var inputHosturl: String
-    lateinit var inputWhitelist: String
-    lateinit var inputUserAgent: String
+class JsonConfigData {
+    lateinit var width_: String
+    lateinit var height_: String
+    lateinit var zoom_factor_: String
+    lateinit var url_: String
+    lateinit var user_agent_: String
+    lateinit var white_list_: String
+    lateinit var block_list_: String
+    lateinit var forces_cale_: String
+    lateinit var embed_cookie_: String
+    lateinit var mobile_page_: String
 
-    fun setUIJsonData() {
 
+
+    fun setUIJsonData(width : String,
+                      height : String,
+                      zoom_factor : String,
+                      url : String,
+                      user_agent : String,
+                      white_list : String,
+                      block_list : String,
+                      forces_cale : String,
+                      embed_cookie: String,
+                      mobile_page : String) {
+        width_ = width
+        height_ = height
+        zoom_factor_ = zoom_factor
+        url_ = url
+        user_agent_ = user_agent
+        white_list_ = white_list
+        block_list_ = block_list
+        forces_cale_ = forces_cale
+        embed_cookie_ = embed_cookie
+        mobile_page_ = mobile_page
     }
 
-    fun parsingJsonData(json : String) {
+    fun parsingJsonData(json : JsonConfigData) {
 
     }
-    fun MakeJsonData(json : String) {
+    fun MakeJsonData(json : JsonConfigData) {
 
     }
 }
--- customtab/app/src/main/java/com/roya/customtab/JsonConfigListActivity.kt
+++ customtab/app/src/main/java/com/roya/customtab/JsonConfigListActivity.kt
@@ -1,6 +1,7 @@
 package com.roya.customtab
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,22 @@ import android.widget.TextView
 
 
 class JsonConfigListActivity : AppCompatActivity() {
+    private lateinit var txtviewJson: TextView
+    private lateinit var btnJsonOk: Button
+    //private lateinit var JsonConfigData1: JsonConfigData
+    private val JsonConfigDataArr = arrayOfNulls<JsonConfigData>(5)
+/*
+    fun setUIJsonData(width : String,
+                      height : String,
+                      zoom_factor : String,
+                      url : String,
+                      user_agent : String,
+                      white_list : String,
+                      block_list : String,
+                      forces_cale : String,
+                      embed_cookie: String,
+                      mobile_page : String) */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,5 +36,61 @@ class JsonConfigListActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initListValue()
     }
+    fun initListValue() {
+        JsonConfigDataArr[1]!!.setUIJsonData("","",
+            "1.5",
+            "https://m.youtube.com",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+            "https://m.youtube.com;https://accounts.google.com;https://accounts.youtube.com;https://www.google.com/recaptcha;https://youtube.com/signin;https://gds.google.com",
+            "",
+            "false",
+            "false",
+            "false",
+        );
+        JsonConfigDataArr[2]!!.setUIJsonData("","",
+            "1",
+            "https://www.hotstar.com",
+            "Mozilla/5.0 (X11; ccNC; Linux aarch64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36",
+            "https://www.hotstar.com/in",
+            "",
+            "false",
+            "false",
+            "false",
+        );
+        JsonConfigDataArr[3]!!.setUIJsonData("","",
+            "2",
+            "https://open.spotify.com",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+            "https://open.spotify.com;https://accounts.google.com;https://www.facebook.com/login.php?;" +
+                    "https://appleid.apple.com/auth/;https://accounts.spotify.com;https://accounts.spotify.com;" +
+                    "https://policies.google.com/privacy;https://policies.google.com/terms;https://support.spotify.com;https://www.spotify.com",
+            "",
+            "false",
+            "false",
+            "true",
+        );
+        JsonConfigDataArr[4]!!.setUIJsonData("","",
+            "1",
+            "https://music.amazon.com",
+            "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+            "https://music.amazon.com;https://www.amazon.com",
+            "",
+            "false",
+            "false",
+            "true",
+        );
+        JsonConfigDataArr[5]!!.setUIJsonData("","",
+            "2",
+            "https://m.cricbuzz.com\n",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+            "https://m.cricbuzz.com;https://auth.cricbuzz.com;https://accounts.google.com;https://appleid.apple.com/auth/",
+            "",
+            "false",
+            "false",
+            "true",
+        );
+    }
+
 }
