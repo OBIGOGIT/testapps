ee488999001c5ab0a199ed782858bda70d39a14f
roya song <roya.song@obigo.com>
2026-01-15 11:15:28 +0900

    webview

--- webview/app/src/main/java/com/example/webview/MainActivity.kt
+++ webview/app/src/main/java/com/example/webview/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.webview
 
+import android.app.Activity
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : Activity() {
+    private var mWebView: WebView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.testshell_activity)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        mWebView = findViewById(R.id.wv1);
+
     }
 }
